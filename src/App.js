eef50df76bebc779a5483a960d708b94fc312844
Jerin <jerin.kuriakose@dreamorbit.com>
2021-11-17 13:26:02 +0530

    Assignment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useEffect, useState } from "react";
+import { ExternalLink } from "react-external-link";
+const PAGE_Number = 1;
 function App() {
+  const [list, setList] = useState([]);
+  const [page, setPage] = useState(PAGE_Number);
+  // calling API using useEffect react hook lifecycle
+  const getApiCall = async () => {
+    const data = await fetch(
+      `https://api.github.com/repos/neovim/neovim/pulls?page=${page}&per_page=30`
+    );
+    const response = await data.json();
+    setList(response);
+  };
+  useEffect(() => {
+    getApiCall();
+  }, [page]);
+
+  const scrolllToEnd = () => {
+    setPage(page + 1);
+  };
+  window.onscroll = function () {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      scrolllToEnd();
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1> -----GitHub User Data ------ </h1>
+
+      <div className="data">
+        <table className="table table table-bordered ">
+          <thead>
+            <tr>
+              <th>Title</th>
+              <th>Base Branch</th>
+              <th>Author Branch</th>
+              <th>Author</th>
+              <th>Created On</th>
+              <th> Labels</th>
+              <th> Details</th>
+            </tr>
+          </thead>
+          <tbody>
+            {list.map((item) => {
+              return (
+                <tr>
+                  <td>{item.title}</td>
+                  <td>{item.base.ref}</td>
+                  <td>{item.author_association}</td>
+                  <td>{item.user.login}</td>
+                  <td>{item.created_at}</td>
+                <td>
+                    {item.labels.map((labelsItem) => {
+                      return (
+                        <ul>
+                          <li>{labelsItem.name}</li>
+                        </ul>
+                      );
+                    })}
+                  </td>
+                  <td>
+                    <ExternalLink href={item.html_url} />
+                  </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
